1fb6b3a8cc8b8061c20924bf80e3a159f273c516
Shane Martin <dev.sh@nemart.in>
2016-02-05 13:38:17 -0500

    Add test

--- public/js/tests/test_utils.js
+++ public/js/tests/test_utils.js
@@ -1,3 +1,14 @@
 QUnit.test("getParticipantId, with window prompt", function(assert) {
   assert.ok(getParticipantId() >= 9999910000 );
 });
+
+
+QUnit.test("createTextBlock", function(assert) {
+  var arg = "<p>Foo bar</p>";
+  var expected = {
+    "type": "text",
+    "text": arg + continue_html,
+    "cont_key": [32, 13]
+  };
+  assert.deepEqual(createTextBlock(arg), expected);
+});
