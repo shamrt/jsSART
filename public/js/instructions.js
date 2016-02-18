de3722eef76caf20deb17b888df391a262bb3ee4
Shane Martin <dev.sh@nemart.in>
2016-02-18 11:12:21 -0500

    Tweak instructions copy

--- public/js/instructions.js
+++ public/js/instructions.js
@@ -9,12 +9,12 @@ var instructions_block = {
   type: "instructions",
   pages: [
     "<p>Welcome!</p> " +
-    "<p>In this study you will be presented with a single digit (1-9) in varying sizes in the middle of the screen for a short duration. The digit is then followed by a crossed circle.</p>",
+    "<p>In this study you will be presented with a single digit (<code>1-9</code>) in varying sizes in the middle of the screen for a short duration. The digit is then followed by a crossed circle.</p>",
 
     "<p>Your task is to either:</p> " +
     "<ol>" +
-      "<li>press the " + spacebar_html + " when you see any digit other than <code>3</code>; or,</li>" +
-      "<li>don’t do anything (press no key) when you see the digit <code>3</code>, and just wait for the next digit to be shown.</li>" +
+      "<li>Press the " + spacebar_html + " when you see any digit other than <code>3</code>.</li>" +
+      "<li>Don’t do anything (press no key) when you see the digit <code>3</code>, and just wait for the next digit to be shown.</li>" +
     "<ol>",
 
     "<p>It’s important to be accurate and fast in this study.</p>" +
