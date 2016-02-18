4f549559513f824b254a68e5917473ea09e02bf8
Shane Martin <dev.sh@nemart.in>
2016-02-18 09:51:53 -0500

    Fix data collection of jspsych-survey-* plugin responses

--- app.js
+++ app.js
@@ -81,7 +81,8 @@ app.post('/experiment-data', function(request, response) {
       "participant_id",
       "num_trials", "trials_per_block", "practice_condition",
       "stimulus", "key_press", "rt",
-      "correct", "response", "expected"
+      "correct", "response", "expected",
+      "responses"  // jspsych-survey-*
     ],
     file_path = ["data", request.body.pathname, request.body.filename + ".csv"].join('/');
   // devise data file path
