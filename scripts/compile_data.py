Update RT average to only count RTs from correct trials

scripts/compile_data.py
@@ -291,6 +291,19 @@ def _calculate_nogo_error_rt_avgs(df):
     }
 
 
+def _get_correct_rts(df):
+    """Take pandas dataframe representing raw SART trails data and
+    return array of RTs for correct trials.
+    """
+    rts = []
+    for idx, series in df.iterrows():
+        if series['correct']:
+            rt = _format_rts([series['rt']])
+            if rt:
+                rts.append(rt[0])
+    return rts
+
+
 def summarize_block_performance(df):
     """Take pandas dataframe representing raw SART trails data and
     summarize performance. Return dict.
@@ -300,10 +313,6 @@ def summarize_block_performance(df):
     # number of trials
     num_trials = len(df.index.values)
     performance['num_trials'] = num_trials
-
-    # average reaction time
-    rts = _format_rts(df['rt'].values)
-    performance['rt_avg'] = round(np.mean(rts), ROUND_NDIGITS)
 
     # add anticipation errors and re-calculate `correct` column
     df = _add_anticipation_errors(df)
@@ -330,6 +339,10 @@ def summarize_block_performance(df):
     performance['nogo_num_errors'] = nogo_errors.count(True)
     nogo_errors_prop = (float(nogo_errors.count(True)) / num_trials)
     performance['nogo_errors'] = round(nogo_errors_prop, ROUND_NDIGITS)
+
+    # average reaction time (RT)
+    correct_rts = _get_correct_rts(df)
+    performance['rt_avg'] = round(np.mean(correct_rts), ROUND_NDIGITS)
 
     # average RTs before and after no-go errors
     nogo_adjacent_rts = _calculate_nogo_error_rt_avgs(df)

scripts/tests/test_compile_data.py
@@ -154,8 +154,8 @@ def test__format_rts():
 def get_sart_trial_block(pid, block_index=0):
     df = get_csv_as_df('experiment', pid)
     blocks = compile_data.extract_sart_blocks(df, with_survey=True)
-    b1 = blocks[block_index]
-    trial_block = b1.loc[b1['trial_type'] == 'multi-stim-multi-response']
+    b = blocks[block_index]
+    trial_block = b.loc[b['trial_type'] == 'multi-stim-multi-response']
     return trial_block
 
 
@@ -235,12 +235,30 @@ def test__calculate_nogo_error_rt_avgs_011():
     assert adjacent_rts['num_next4_rts'] == 8
 
 
+def test__get_correct_rts_blk1():
+    pid = "104"
+    sart_block = get_sart_trial_block(pid)
+    df = compile_data._add_anticipation_errors(sart_block)
+    rts = compile_data._get_correct_rts(df)
+    assert len(rts) == 71
+    assert compile_data.np.mean(rts) == 288.6056338028169
+
+
+def test__get_correct_rts_blk4():
+    pid = "104"
+    sart_block = get_sart_trial_block(pid, 3)
+    df = compile_data._add_anticipation_errors(sart_block)
+    rts = compile_data._get_correct_rts(df)
+    assert len(rts) == 49
+    assert compile_data.np.mean(rts) == 353.36734693877548
+
+
 def test_summarize_block_performance():
     pid = "104"
     sart_block = get_sart_trial_block(pid)
     p = compile_data.summarize_block_performance(sart_block)
     assert p['num_trials'] == 82
-    assert p['rt_avg'] == 272.790123457
+    assert p['rt_avg'] == 288.605633803
     assert p['anticipated'] == 0.073170732  # 6 anticipation errors
     assert p['go_errors'] == 0.012195122  # 1 go error
     assert p['nogo_num_errors'] == 4